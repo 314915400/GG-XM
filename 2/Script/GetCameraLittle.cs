f8700da9924b43d136ee5390d5938d377184ae81
Nexoriu <2832158672@qq.com>
2025-06-28 14:49:36 +0800

    2

--- 2/Script/GetCameraLittle.cs
+++ 2/Script/GetCameraLittle.cs
@@ -4,134 +4,92 @@ using System.Drawing;
 public class GetCameraLittle : MonoBehaviour
 {
     public Camera camera;
-    [Header("房间位置")]
-    public Transform fangjian1;
-    public Transform fangjian2;
-    public Transform fangjian3;
-    public Transform fangjian4;
-    public Transform fangjian5;
-    [Header("场景物体")]
-    public GameObject bingxiang;
-    public GameObject Door1;
-    public GameObject Door2;
-    public GameObject Door3;
-    public GameObject Door4;
-    [Header("代码引用")]
-    public Door1 door1;
-    public Door2 door2;
-    public Door3 door3;
-    public Door4 door4;
+    public Transform chushikongjian;
+    public Transform zhiwu;
+    private bool isfollow1=false;
+    private bool isfollow2=false;
+    public GameObject shoudiantong;
+    public Transform genjianzuopiao;
+    public Transform matongzuobiao;
+    public GameObject matong;
+    float juli = 0;
     private void Awake()
     {
         camera = GetComponent<Camera>();
         GetLittle();
-        
+    }
+    private void Start()
+    {
+        matong.GetComponent<Animator>().enabled = false;
     }
     private void Update()
     {
-        if (camera.orthographicSize>3.29)
+        if (camera.orthographicSize>0.93)
         camera.orthographicSize -= Time.deltaTime * 10;
-        if(door1.isEnd1)
+        if (isfollow1)
         {
-        YidongcameraTo2();
+            CameraFollow2();
         }
-        else if(door2.isEnd2)
-        {
-            YidongcameraTo3();
+        if (isfollow2)
+        {         
+        Mouse2yidong();
         }
-        else if(door3.isEnd3)
-        {
-            YidongcameraTo4();
-        }
+        matongLight();
     }
     public void GetLittle()
     {
-        Vector3 fangjian1Position= fangjian1.position;
+        Vector3 fangjian1Position= chushikongjian.position;
         fangjian1Position.z = -10;
-        camera.transform.DOPath(new Vector3[] { camera.transform.position, fangjian1Position}, 1)
-            .SetEase(Ease.OutQuad)
+        camera.transform.DOPath(new Vector3[] { camera.transform.position, fangjian1Position}, 3)
+           .SetEase(Ease.OutQuad)
              .OnComplete(
             () =>
             {
-                //TODO:激活UI物体
-                bingxiang.SetActive(true);
-                Door1.SetActive(true);
+                Invoke("Shengchengshoudiantong", 0.2f);
             }
             );
     }
-    public void YidongcameraTo2()
+    public void Shengchengshoudiantong()
     {
-        
-            Vector3 fangjian2Position = fangjian2.position;
-            fangjian2Position.z = -10;
-            camera.transform.DOPath(new Vector3[] { camera.transform.position , fangjian2Position },1)
-                .SetEase(Ease.OutQuad)
-                 .OnComplete(
-            () =>
-            {
-                //TODO:失活上一个场景物体
-                bingxiang.SetActive(false);
-                Door1.SetActive(false);
-                //TODO:激活UI物体
-                Door2.SetActive(true);
+        shoudiantong.SetActive(true);
+    }
+    public void shoudiantongONbutton()
+    {
+        CameraFollow1();
+        isfollow2 = true;
+    }
+    public void Mouse2yidong()
+    {
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            MousePosition.z = -10;
+            camera.transform.position = MousePosition;
+        }
 
-            }
-            );
-        
     }
-    public void YidongcameraTo3()
+    public void CameraFollow1()
     {
-        
-            Vector3 fangjian3Position = fangjian3.position;
-            fangjian3Position.z = -10;
-            camera.transform.DOPath(new Vector3[] { camera.transform.position , fangjian3Position },1)
-                .SetEase(Ease.OutQuad)
-                 .OnComplete(
-            () =>
-            {
-                //TODO:失活上一个场景物体
-                Door2.SetActive(false);
-                //TODO:激活UI物体
-                Door3.SetActive(true);
-            }
-            );
-        
+        Vector3 zhiwuPosition = zhiwu.position;
+        zhiwuPosition.z = -10;
+        camera.transform.DOPath(new Vector3[] { camera.transform.position, zhiwuPosition }, 1)
+           .SetEase(Ease.OutQuad);
+        isfollow1=true;
     }
-    public void YidongcameraTo4()
+    public void CameraFollow2()
     {
-        
-            Vector3 fangjian4Position = fangjian4.position;
-            fangjian4Position.z = -10;
-            camera.transform.DOPath(new Vector3[] { camera.transform.position , fangjian4Position },1)
-                .SetEase(Ease.OutQuad)
-                 .OnComplete(
-            () =>
-            {
-                //TODO:失活上一个场景物体
-                Door3.SetActive(false);
-                //TODO:激活UI物体
-                Door4.SetActive(true);
-
-            }
-            );
-        
+        Vector3 zhiwuPosition = zhiwu.position;
+        zhiwuPosition.z = -10;
+        camera.transform.position = zhiwuPosition;
+        camera.orthographicSize = 2.57f;
     }
-    public void YidongcameraTo5()
+    public void jiancejuli()
     {
-        
-            Vector3 fangjian5Position = fangjian5.position;
-            fangjian5Position.z = -10;
-            camera.transform.DOPath(new Vector3[] { camera.transform.position , fangjian5Position },1)
-                .SetEase(Ease.OutQuad)
-                 .OnComplete(
-            () =>
-            {
-                //TODO:失活上一个场景物体
-                Door4.SetActive(false);
-                //TODO:激活UI物体
-
-            }
-            );
-        
+        float juli = Vector3.Distance(matongzuobiao.position, camera.transform.position);    }
+    public void matongLight()
+    {
+        jiancejuli();
+        if (juli<=1.6)
+            matong.GetComponent<Animator>().enabled = true;
     }
 }
