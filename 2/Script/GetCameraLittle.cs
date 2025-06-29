fec5d24dfd0713f84b1e1ce687c433613ec80311
Nexoriu <2832158672@qq.com>
2025-06-29 12:22:13 +0800

    1

--- 2/Script/GetCameraLittle.cs
+++ 2/Script/GetCameraLittle.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
-using System.Drawing;
 using UnityEngine.UI;
-using Unity.VisualScripting;
 public class GetCameraLittle : MonoBehaviour
 {
     public Camera camera;
@@ -16,16 +14,12 @@ public class GetCameraLittle : MonoBehaviour
     public Transform matongzuobiao;
     public GameObject matong;
     public GameObject zhuchangjing ;
+    public 物体基类 wutijilei;
+    public bool isAnimation=false;
     ////////////////////////////////////////////////////////////////////////////////////////////
     [Header("第一组物体")]
     public GameObject sliderwuti1;
     public GameObject sliderwuti2;
-    //[Header("第二组物体")]
-    //public GameObject sliderwuti3;
-    //public GameObject sliderwuti4;
-    //[Header("第三组物体")]
-    //public GameObject sliderwuti5;
-    //public GameObject sliderwuti6;
     [Header("第一组滑块")]
     public Slider slider1; // 拖拽 Slider 组件到这个变量
     public Transform cameraTransform1; // 拖拽需要移动的摄像头的 Transform 组件到这个变量
@@ -37,87 +31,39 @@ public class GetCameraLittle : MonoBehaviour
     public Vector3 moveDirection2 = Vector3.forward; // 设置摄像头移动的方向，默认为向前移动
     public float minDistance2 = 0f; // 设置摄像头移动的最小距离
     public float maxDistance2 = 10f; // 设置摄像头移动的最大距离
-    //[Header("第二组滑块")]
-    //public Slider slider3; // 拖拽 Slider 组件到这个变量
-    //public Transform cameraTransform3; // 拖拽需要移动的摄像头的 Transform 组件到这个变量
-    //public Vector3 moveDirection3 = Vector3.forward; // 设置摄像头移动的方向，默认为向前移动
-    //public float minDistance3 = 0f; // 设置摄像头移动的最小距离
-    //public float maxDistance3 = 10f; // 设置摄像头移动的最大距离
-    //public Slider slider4; // 拖拽 Slider 组件到这个变量
-    //public Transform cameraTransform4; // 拖拽需要移动的摄像头的 Transform 组件到这个变量
-    //public Vector3 moveDirection4 = Vector3.forward; // 设置摄像头移动的方向，默认为向前移动
-    //public float minDistance4 = 0f; // 设置摄像头移动的最小距离
-    //public float maxDistance4= 10f; // 设置摄像头移动的最大距离
-
-    //[Header("第三组滑块")]
-    //public Slider slider5; // 拖拽 Slider 组件到这个变量
-    //public Transform cameraTransform5; // 拖拽需要移动的摄像头的 Transform 组件到这个变量
-    //public Vector3 moveDirection5 = Vector3.forward; // 设置摄像头移动的方向，默认为向前移动
-    //public float minDistance5 = 0f; // 设置摄像头移动的最小距离
-    //public float maxDistance5 = 10f; // 设置摄像头移动的最大距离
-    //public Slider slider6; // 拖拽 Slider 组件到这个变量
-    //public Transform cameraTransform6; // 拖拽需要移动的摄像头的 Transform 组件到这个变量
-    //public Vector3 moveDirection6 = Vector3.forward; // 设置摄像头移动的方向，默认为向前移动
-    //public float minDistance6 = 0f; // 设置摄像头移动的最小距离
-    //public float maxDistance6 = 10f; // 设置摄像头移动的最大距离
 
 
     private void Start()
     {
         zhuchangjing.GetComponent<Animator>().enabled = false;
-        // 初始化 Slider 的值范围
+         //初始化 Slider 的值范围
         slider1.minValue = minDistance1;
         slider1.maxValue = maxDistance1;
-        slider1.value = (minDistance1 + maxDistance1) / 2; 
-        // 设置初始值为中间值
+        slider1.value = (minDistance1 + maxDistance1) / 2; // 设置初始值为中间值
         slider2.minValue = minDistance2;
         slider2.maxValue = maxDistance2;
-        slider2.value = (minDistance2 + maxDistance2) / 2;
-        //// 设置初始值为中间值
-        //slider3.minValue = minDistance3;
-        //slider3.maxValue = maxDistance3;
-        //slider3.value = (minDistance3 + maxDistance3) / 2; 
-        //// 设置初始值为中间值
-        //slider4.minValue = minDistance4;
-        //slider4.maxValue = maxDistance4;
-        //slider4.value = (minDistance4 + maxDistance4) / 2; 
-        //// 设置初始值为中间值
-        //slider5.minValue = minDistance5;
-        //slider5.maxValue = maxDistance5;
-        //slider5.value = (minDistance5 + maxDistance5) / 2; 
-        //// 设置初始值为中间值
-        //slider6.minValue = minDistance6;
-        //slider6.maxValue = maxDistance6;
-        //slider6.value = (minDistance6 + maxDistance6) / 2; 
+        slider2.value = (minDistance2 + maxDistance2)/2;      // 设置初始值为中间值
 
     }
 
-    // 当 Slider 值改变时调用此方法
+    //当 Slider 值改变时调用此方法
     public void OnSliderValueChanged()
     {
         UpdateCameraPosition();
     }
 
-    // 根据 Slider 值更新摄像头位置
+    //根据 Slider 值更新摄像头位置
     private void UpdateCameraPosition()
     {
         float distance1 = slider1.value; // 获取 Slider 当前值
         cameraTransform1.position = transform.position + moveDirection1.normalized * distance1; // 根据方向和距离设置摄像头位置
         float distance2 = slider2.value; // 获取 Slider 当前值
-        //cameraTransform2.position = transform.position + moveDirection2.normalized * distance2; // 根据方向和距离设置摄像头位置
-        //float distance3 = slider1.value; // 获取 Slider 当前值
-        //cameraTransform1.position = transform.position + moveDirection3.normalized * distance3; // 根据方向和距离设置摄像头位置
-        //float distance4 = slider2.value; // 获取 Slider 当前值
-        //cameraTransform2.position = transform.position + moveDirection4.normalized * distance4; // 根据方向和距离设置摄像头位置
-        //float distance5 = slider1.value; // 获取 Slider 当前值
-        //cameraTransform1.position = transform.position + moveDirection5.normalized * distance5; // 根据方向和距离设置摄像头位置
-        //float distance6 = slider2.value; // 获取 Slider 当前值
-        //cameraTransform2.position = transform.position + moveDirection6.normalized * distance6; // 根据方向和距离设置摄像头位置
+        cameraTransform2.position = transform.position + moveDirection2.normalized * distance2; // 根据方向和距离设置摄像头位置
     }
 
-/// <summary>
-/// ////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// </summary>
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// </summary>
     private void Awake()
     {
         camera = GetComponent<Camera>();
@@ -133,9 +79,13 @@ public class GetCameraLittle : MonoBehaviour
         {
             CameraFollow2();
         }
+
         if (isfollow2)
         {         
         Mouse2yidong();
+
+       UpdateCameraPosition();
+
         }
     }
     public void GetLittle()
@@ -155,12 +105,18 @@ public class GetCameraLittle : MonoBehaviour
     {
         shoudiantong1.SetActive(true);
     }
+    public void tranTotrue()
+    {
+        if(isAnimation)
+        wutijilei.xxx();
+    }
+        
     public void shoudiantongONbutton()
     {
         CameraFollow1();
         shoudiantong3.SetActive(true);
         shoudiantong1.SetActive(false);
-        isfollow2 = true;
+        isfollow1 = true;
     }
     public void Mouse2yidong()
     {
@@ -170,27 +126,30 @@ public class GetCameraLittle : MonoBehaviour
             MousePosition.z = -10;
             camera.transform.position = MousePosition;
             matong.SetActive(true);
-            sliderwuti1.SetActive(true);
-            sliderwuti2.SetActive(true);
         }
     }
-
     public void CameraFollow1()
     {
         Vector3 zhiwuPosition = zhiwu.position;
         zhiwuPosition.z = -10;
         camera.transform.DOPath(new Vector3[] { camera.transform.position, zhiwuPosition }, 1)
            .SetEase(Ease.OutQuad);
-        isfollow1=true;
-
+       //Invoke("tranfollow1true", 2);
+        tranfollow1true();
+        sliderwuti1.SetActive(true);
+        sliderwuti2.SetActive(true);
     }
+
+    private void tranfollow1true()
+    {
+        isfollow1 = true;
+    }
+
     public void CameraFollow2()
     {
-        Vector3 zhiwuPosition = zhiwu.position;
-        zhiwuPosition.z = -10;
-        camera.transform.position = zhiwuPosition;
-        camera.orthographicSize = 3f;
-        UpdateCameraPosition();
-
+        Vector3 zhiwuPosition2 = zhiwu.position;
+        zhiwuPosition2.z = -10;
+        Camera.main.transform.position = zhiwuPosition2;
+        isfollow2 = true;
     }
 }
